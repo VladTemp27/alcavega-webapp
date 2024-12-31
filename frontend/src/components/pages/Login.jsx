df92885d4c31556c678baa7a03fc63cce9067097
Lestat <2233915@slu.edu.ph>
2024-12-31 16:20:52 +0800

    Implement login functionality with form handling and API integration

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -1,32 +1,69 @@
-import { Stack, HStack, VStack ,Box, Image, Flex } from '@chakra-ui/react'
-import { Input,Button } from '@chakra-ui/react'
 import {
+    Stack,
+    HStack,
+    VStack,
+    Box,
+    Image,
+    Flex,
+    Input,
+    Button,
     FormControl,
     FormLabel,
     FormErrorMessage,
     FormHelperText,
-  } from '@chakra-ui/react'
+    Card,
+    CardBody,
+    CardHeader,
+    CardFooter,
+    Heading
+} from '@chakra-ui/react';
+import axios from 'axios'
+import React from 'react';
 
-  import { Card, CardBody, CardHeader, CardFooter, Heading } from '@chakra-ui/react'
+async function handleLogin(event, username, password){
+    event.preventDefault();
+    try{
+        const response = await axios.post('http://localhost:8080/auth/login', {
+            username,
+            password
+        });
+        console.log(response.data);
+        sessionStorage.setItem('token', response.data.token);
+        console.log('Token saved to session storage');
+    }catch(error){
+        console.error(error);
+    }
+}
 
-function LoginForm(){
+function LoginForm({onSubmit}){
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
     return(
         <Card minW={{ base: '40%'}}>
             <CardHeader>
                 <Heading as='h2' size='lg'>Log-in</Heading>
             </CardHeader>
             <CardBody>
-                <FormControl isRequired>
+                <FormControl as="form" isRequired onSubmit={(event) => onSubmit(event, username, password)}>
                     <FormLabel>Username</FormLabel>
-                    <Input placeholder="Username" />
+                    <Input
+                        placeholder="Username" 
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                    />
                     <FormLabel>Password</FormLabel>
-                    <Input placeholder='Password'/>
+                    <Input 
+                        placeholder='Password'
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
                     <Button
                     mt={4}
                     colorScheme='teal'
                     type='submit'
                 >
-                    Submit
+                    Log-in
                 </Button>
                 </FormControl>
             </CardBody>
@@ -51,7 +88,7 @@ function Login() {
                 alt='Alcavega'
                 marginBottom={4}
             />
-            <LoginForm/>
+            <LoginForm onSubmit={handleLogin}/>
         </Flex>
     </Box>
   );
