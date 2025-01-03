a114de74ef89b894eee74ac21ed314754e75007f
Lestat <2233915@slu.edu.ph>
2025-01-03 20:12:50 +0800

    Add crop routes for CRUD operations: get, create, update, and delete crops

---
+++ backend/routes/crop-routes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import CropController from '../controller/crop-controller.js'
+
+const cropRoutes = express.Router()
+
+cropRoutes.use(express.json())
+
+cropRoutes.get('/crops', CropController.getCrops)
+cropRoutes.post('/crop', CropController.createCrop)
+cropRoutes.put('/crop/:crop_id', CropController.updateCrop)
+cropRoutes.delete('/crop/:crop_id', CropController.deleteCrop)
+
+export default cropRoutes
