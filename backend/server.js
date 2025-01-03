bd9995af547b866379ba6dd2d89fa5be07c3ddbe
Lestat <2233915@slu.edu.ph>
2025-01-03 12:00:02 +0800

    Add farmer routes to server for API integration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {connectDB} from './config/db.js';
 import userRoutes from './routes/user-routes.js';
+import farmerRoutes from './routes/farmer-routes.js';
 import authRoutes from './services/auth/auth-routes.js';
 import authenticateToken from './services/auth/auth-middleware.js';
 import dotenv from 'dotenv'
@@ -19,6 +20,7 @@ app.get('/', async (req, res) => {
 
 app.use('/auth', authRoutes);
 app.use('/api/user-service', authenticateToken(true),userRoutes);
+app.use('/api/farmer-service', authenticateToken(true),farmerRoutes);
 
 app.listen(8080, async () => {
     await connectDB();
