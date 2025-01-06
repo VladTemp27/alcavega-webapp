2d306ded1ad3afa6841c4ac92d2d32726ad32a9a
Lestat <2233915@slu.edu.ph>
2025-01-06 14:48:15 +0800

    Added new routes for editing and getting buyers

--- backend/routes/crop-routes.js
+++ backend/routes/crop-routes.js
@@ -9,5 +9,7 @@ cropRoutes.get('/crops', CropController.getCrops)
 cropRoutes.post('/crop', CropController.createCrop)
 cropRoutes.put('/crop/:crop_id', CropController.updateCrop)
 cropRoutes.delete('/crop/:crop_id', CropController.deleteCrop)
+cropRoutes.get('/buyers/:crop_id',CropController.getBuyers)
+cropRoutes.put('/buyers',CropController.editBuyers)
 
 export default cropRoutes
