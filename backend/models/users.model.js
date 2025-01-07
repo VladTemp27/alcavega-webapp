5c68ca468564ff0bd728e0fee3eae1dc7221017e
Lestat <2233915@slu.edu.ph>
2025-01-07 14:06:18 +0800

    Implement password hashing and comparison methods in user model

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'
 
 const userSchema = mongoose.Schema({
     username:{
@@ -42,6 +43,25 @@ const userSchema = mongoose.Schema({
         required: true
     }
 });
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')|| !this.isNew) {
+        next()   
+        return
+    }
+
+    try{
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt)
+        next();
+    }catch(error){
+        next(error);
+    }
+});
+
+userSchema.methods.comparePassword = async function(inputPassword){
+    return await bcrypt.compare(inputPassword, this.password)
+}
 
 
 const User = mongoose.model('User', userSchema);
