26dd5f245afc4d30a89260778a35697d5afbbfeb
Lestat <2233915@slu.edu.ph>
2025-08-07 12:44:24 +0800

    fix: establish MongoDB connection before starting the server

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-
+import mongoUtil from './v1/util/mongoUtil';
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 2000;
 
 try{
+    await mongoUtil.connect();
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
